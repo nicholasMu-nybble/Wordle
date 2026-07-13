d13e2d58b0efead05b798ae327106bf484ba4524
nicholasMu-nybble <10978346@uvu.edu>
2026-07-13 14:53:48 -0600

    create main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("test")
+}
+
+/*
+randomly select a 5 letter word
+
+input 5 letter word
+
+display word
+
+show status of each letter
+
+limit to 6 words, then show word
+*/
